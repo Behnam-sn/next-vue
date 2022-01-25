eff929a4c9399d48e864b7a209cd821eb098b091
Behnam Salehi <salehinezhad.b@gmail.com>
2022-01-25 23:20:16 +0330

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { useCounterStore } from "@/stores/counter";
+
+const counter = useCounterStore();
+let a = "ca";
+</script>
+
+<template>
+  <main class="bg-red-300">
+    <div>home {{ counter.counter }}</div>
+    <button @click="counter.increment">add</button>
+    <div>{{ a }}</div>
+    <div>{{ counter.doubleCount }}</div>
+  </main>
+</template>
