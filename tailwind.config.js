25a16e01dd5e269aee0f959863772bfe4605fba5
Behnam Salehi <salehinezhad.b@gmail.com>
2022-01-28 06:24:16 +0330

    format code style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          900: "#020102",
+        },
+        secondary: "#ffffff",
+      },
+      fontFamily: {
+        Ubuntu: ["'Ubuntu'", "sans-serif"],
+        Quicksand: ["'Quicksand'", "sans-serif"],
+        Roboto: ["'Roboto'", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
