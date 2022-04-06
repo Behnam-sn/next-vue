39b51b575d62f65aed6358db977920bf81d8fbd4
Behnam Salehi <salehinezhad.b@gmail.com>
2022-04-06 13:16:15 +0430

    update songs

--- src/stores/songs.ts
+++ src/stores/songs.ts
@@ -1,5 +1,5 @@
 import { defineStore } from "pinia";
-import { Song } from "@/models/song.model";
+import type { Song } from "@/models/song.model";
 
 export type State = {
   songs: Song[];
@@ -11,54 +11,60 @@ export const useSongsStore = defineStore({
     ({
       songs: [
         {
-          artists: ["Billie Eilish"],
+          id: "lUfBssvFSD",
+          artists: ["Billie Eilish", "saeed asayesh"],
           title: "everything i wanted",
-          album: "everything i wanted Single",
+          albumId: "everything i wanted Single",
           number: 0,
           length: "04:05",
           thumbnail: "a7z491lObd.png",
           fileName: "",
         },
         {
+          id: "08ZBlEuJbt",
           artists: ["Imagine Dragons"],
           title: "Believer",
-          album: "Evolve",
+          albumId: "Evolve",
           number: 3,
           length: "03:24",
           thumbnail: "Rnb1YYiXer.jpg",
           fileName: "",
         },
         {
+          id: "Ueu1aVwooq",
           artists: ["Ed Sheeran"],
           title: "Shape of You",
-          album: "Divide",
+          albumId: "Divide",
           number: 4,
           length: "03:53",
           thumbnail: "nGRl72oCIc.png",
           fileName: "",
         },
         {
+          id: "QLknrbUFPQ",
           artists: ["The Weeknd"],
           title: "After Hours",
-          album: "After Hours",
+          albumId: "After Hours",
           number: 13,
           length: "06:01",
           thumbnail: "5xUwktQM79.jpg",
           fileName: "",
         },
         {
+          id: "fgyUTxJdYv",
           artists: ["Drake"],
           title: "God's Plan",
-          album: "Scorpion",
+          albumId: "Scorpion",
           number: 5,
           length: "03:19",
           thumbnail: "0FLrDT3KU7.jpg",
           fileName: "",
         },
         {
+          id: "wIqu7NVPnB",
           artists: ["Billie Eilish"],
           title: "GOLDWING",
-          album: "Happier Than Ever",
+          albumId: "Happier Than Ever",
           number: 6,
           thumbnail: "EVEHF7pGMO.png",
           fileName: "",
