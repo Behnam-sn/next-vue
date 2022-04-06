59eff36b9312fcc918230ee627b770b3a4981cd4
Behnam Salehi <salehinezhad.b@gmail.com>
2022-04-06 09:51:46 +0430

    update packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,12 @@ module.exports = {
   env: {
     "vue/setup-compiler-macros": true,
   },
+  overrides: [
+    {
+      files: ["cypress/integration/**.spec.{js,ts,jsx,tsx}"],
+      extends: ["plugin:cypress/recommended"],
+    },
+  ],
   rules: {
     "no-undef": "off",
     "prettier/prettier": [
