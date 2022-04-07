726978af8e2f931c9978ef86075a80b930d21bd8
Behnam Salehi <salehinezhad.b@gmail.com>
2022-04-07 10:35:36 +0430

    update views

--- src/views/ArtistsView.vue
+++ src/views/ArtistsView.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+import TitleBar from "@/components/TitleBar.vue";
+import ArtistCard from "@/components/ArtistCard.vue";
+
+import { useArtistsStore } from "@/stores/artists";
+
+const artists = useArtistsStore();
+</script>
+
 <template>
-  <div>artists</div>
+  <div>
+    <TitleBar text="artists" />
+    <div class="flex flex-wrap">
+      <div v-for="(artist, index) in artists.artists" :key="index">
+        <ArtistCard :artist="artist" />
+      </div>
+    </div>
+  </div>
 </template>
--- src/views/SongsView.vue
+++ src/views/SongsView.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+import TitleBar from "@/components/TitleBar.vue";
+import SongCard from "@/components/SongCard.vue";
+
+import { useSongsStore } from "@/stores/songs";
+
+const songsStore = useSongsStore();
+</script>
+
 <template>
-  <div>songs</div>
+  <div>
+    <TitleBar text="songs" />
+    <div class="flex flex-wrap">
+      <div v-for="(song, index) in songsStore.songs" :key="index">
+        <SongCard :song="song" />
+      </div>
+    </div>
+  </div>
 </template>
