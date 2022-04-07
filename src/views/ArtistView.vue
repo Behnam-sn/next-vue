64c93ed5abc3f70daf6ffc6762bf04b00b46f0af
Behnam Salehi <salehinezhad.b@gmail.com>
2022-04-07 10:36:20 +0430

    add artist view

---
+++ src/views/ArtistView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+import { useRoute } from "vue-router";
+import { useArtistsStore } from "@/stores/artists";
+
+const route = useRoute();
+const id: any = route.params.id;
+
+const artistsStore = useArtistsStore();
+const artist = ref(artistsStore.getArtistById(id));
+</script>
+
+<template>
+  <div>{{ artist?.name }}</div>
+  <div>{{ artist?.monthlyListeners }}</div>
+</template>
