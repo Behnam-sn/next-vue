5e2c27ead01aec474b76a91a4fc31dd7f2303d42
Behnam Salehi <salehinezhad.b@gmail.com>
2022-04-08 20:37:31 +0430

    update scrollBehavior

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,13 @@ import NProgress from "nprogress";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
   routes: [
     {
       path: "/",
