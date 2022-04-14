bb2f5d3f443e1d46e9196a7c167724943cd281b0
Behnam Salehi <salehinezhad.b@gmail.com>
2022-04-14 22:57:28 +0430

    add liked songs shelf

---
+++ src/components/Shelfs/LikedSongsShelf.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+import SongCard from "@/components/Cards/SongCard.vue";
+
+import { useUserStore } from "@/stores/user";
+const userStore = useUserStore();
+</script>
+
+<template>
+  <swiper :slides-per-view="'auto'">
+    <swiper-slide
+      v-for="song in userStore.likedSongs.slice().reverse()"
+      :key="song.id"
+    >
+      <SongCard :song="song" />
+    </swiper-slide>
+  </swiper>
+</template>
+
+<style>
+.swiper-slide {
+  width: auto !important;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,7 @@ import SongShelf from "@/components/Shelfs/SongShelf.vue";
 import ArtistShelf from "@/components/Shelfs/ArtistShelf.vue";
 import AlbumShelf from "@/components/Shelfs/AlbumShelf.vue";
 import RecentsShelf from "@/components/Shelfs/RecentsShelf.vue";
+import LikedSongs from "@/components/Shelfs/LikedSongsShelf.vue";
 </script>
 
 <template>
@@ -18,5 +19,7 @@ import RecentsShelf from "@/components/Shelfs/RecentsShelf.vue";
     <AlbumShelf />
     <TitleBar text="recents" />
     <RecentsShelf />
+    <TitleBar text="liked songs" />
+    <LikedSongs />
   </main>
 </template>
