003f24aa1b458f78e2fec8d738a70db72aa4fa1c
behnam <salehinezhad.b@gmail.com>
2022-04-25 00:58:36 +0430

    add navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import BarsIcon from "@/assets/icons/BarsIcon.vue";
+</script>
+
+<template>
+  <div
+    class="fixed top-0 z-10 w-full bg-primary-600 py-6 px-7 text-secondary-900 transition duration-500 xl:hidden"
+  >
+    <div class="flex flex-row items-center justify-between">
+      <router-link to="/">
+        <h1 class="text-2xl">Next</h1>
+      </router-link>
+      <div class="fill-secondary-900">
+        <BarsIcon class="w-7" />
+      </div>
+    </div>
+  </div>
+</template>
