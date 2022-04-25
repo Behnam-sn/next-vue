8026fac86fcb33c04013e05144ea1e0464dbf39f
behnam <salehinezhad.b@gmail.com>
2022-04-26 03:01:16 +0430

    update nav bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,11 +4,11 @@ import BarsIcon from "@/assets/icons/BarsIcon.vue";
 
 <template>
   <div
-    class="fixed top-0 z-10 w-full bg-primary-600 py-6 px-7 text-secondary-900 transition duration-500 xl:hidden"
+    class="fixed top-0 z-10 w-full bg-tertiary-900 py-6 px-7 text-secondary-900 transition duration-500 lg:hidden"
   >
     <div class="flex flex-row items-center justify-between">
       <router-link to="/">
-        <h1 class="text-2xl">Next</h1>
+        <h1 class="font-Ubuntu text-3xl font-normal">Next</h1>
       </router-link>
       <div class="fill-secondary-900">
         <BarsIcon class="w-7" />
