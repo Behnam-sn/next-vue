9de34f028b237f83bd3b7cc75659221ec0b3d6a7
behnam <salehinezhad.b@gmail.com>
2022-05-02 22:04:03 +0430

    add info banner

--- src/views/LikesView.vue
+++ src/views/LikesView.vue
@@ -2,6 +2,8 @@
 import TitleBar from "@/components/TitleBar.vue";
 import SongCard from "@/components/Cards/SongCard.vue";
 
+import HeartCrackIcon from "@/assets/icons/HeartCrackIcon.vue";
+
 import { useUserStore } from "@/stores/user";
 const userStore = useUserStore();
 </script>
@@ -17,7 +19,18 @@ const userStore = useUserStore();
       </div>
     </template>
     <template v-else>
-      <div>No Songs got Liked yet</div>
+      <div
+        class="mx-5 mt-10 flex items-center justify-center rounded-lg bg-primary-800 py-32 transition-colors duration-300 dark:bg-secondary-800 lg:mx-48"
+      >
+        <HeartCrackIcon
+          class="mr-4 h-16 fill-secondary-400 transition-colors duration-300 dark:fill-primary-400"
+        />
+        <p
+          class="text-center font-Roboto text-lg font-medium text-secondary-900 transition-colors duration-300 dark:text-primary-900"
+        >
+          No Song is Liked
+        </p>
+      </div>
     </template>
   </div>
 </template>
--- src/views/RecentsView.vue
+++ src/views/RecentsView.vue
@@ -2,6 +2,8 @@
 import TitleBar from "@/components/TitleBar.vue";
 import SongCard from "@/components/Cards/SongCard.vue";
 
+import HistoryIcon from "@/assets/icons/HistoryIcon.vue";
+
 import { useUserStore } from "@/stores/user";
 const userStore = useUserStore();
 </script>
@@ -17,7 +19,18 @@ const userStore = useUserStore();
       </div>
     </template>
     <template v-else>
-      <div>No Songs played yet</div>
+      <div
+        class="mx-5 mt-10 flex items-center justify-center rounded-lg bg-primary-800 py-32 transition-colors duration-300 dark:bg-secondary-800 lg:mx-48"
+      >
+        <HistoryIcon
+          class="mr-4 h-16 fill-secondary-400 transition-colors duration-300 dark:fill-primary-400"
+        />
+        <p
+          class="text-center font-Roboto text-lg font-medium text-secondary-900 transition-colors duration-300 dark:text-primary-900"
+        >
+          No Songs played yet
+        </p>
+      </div>
     </template>
   </div>
 </template>
