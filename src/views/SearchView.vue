7a4c823b859a54485ede0cdb00042d58a70e569e
behnam <salehinezhad.b@gmail.com>
2022-05-02 21:56:06 +0430

    update responsivity

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -44,22 +44,24 @@ watch(searchText, (newSearchText) => {
 
 <template>
   <div>
-    <div class="px-20">
+    <div class="lg:px-20">
       <div class="relative flex items-center">
-        <SearchIcon class="absolute left-4 h-6 fill-secondary-400" />
+        <SearchIcon
+          class="absolute left-4 h-6 fill-secondary-400 transition-colors duration-300 dark:fill-primary-400"
+        />
         <input
           v-model="searchText"
-          class="w-full rounded-full bg-primary-700 px-12 py-3 font-Quicksand text-secondary-900 outline-none transition duration-300 hover:bg-primary-600 focus:bg-primary-600"
+          class="w-full rounded-full bg-primary-700 px-12 py-4 font-Quicksand text-secondary-900 outline-none transition-colors duration-300 hover:bg-primary-600 focus:bg-primary-600 dark:bg-secondary-700 dark:text-primary-900 dark:hover:bg-secondary-600 dark:focus:bg-secondary-600"
           type="text"
           placeholder="Search"
         />
         <button
           v-show="searchText"
           @click="clearSerach"
-          class="absolute right-6"
+          class="absolute right-4 flex h-9 w-9 items-center justify-center rounded-full bg-primary-400 shadow transition-colors duration-300 dark:bg-secondary-400"
         >
           <CloseIcon
-            class="h-7 fill-secondary-500 transition duration-500 dark:text-gray-600"
+            class="h-7 fill-secondary-500 transition-colors duration-300 dark:fill-primary-500"
           />
         </button>
       </div>
@@ -89,5 +91,38 @@ watch(searchText, (newSearchText) => {
         </swiper-slide>
       </swiper>
     </template>
+
+    <div
+      class="mx-5 mt-10 flex items-center justify-center rounded-lg bg-primary-800 py-32 transition-colors duration-300 dark:bg-secondary-800 lg:mx-48"
+      v-if="!searchText"
+    >
+      <SearchIcon
+        class="mr-4 h-16 fill-secondary-400 transition-colors duration-300 dark:fill-primary-400"
+      />
+      <p
+        class="text-center font-Roboto text-lg font-medium text-secondary-900 transition-colors duration-300 dark:text-primary-900"
+      >
+        Type Something ...
+      </p>
+    </div>
+
+    <div
+      class="mx-5 mt-10 flex items-center justify-center rounded-lg bg-primary-800 py-32 transition-colors duration-300 dark:bg-secondary-800 lg:mx-48"
+      v-if="
+        searchText &&
+        !searchStore.songs.length &&
+        !searchStore.artists.length &&
+        !searchStore.albums.length
+      "
+    >
+      <CloseIcon
+        class="mr-4 h-16 fill-secondary-400 transition-colors duration-300 dark:fill-primary-400"
+      />
+      <p
+        class="text-center font-Roboto text-lg font-medium text-secondary-900 transition-colors duration-300 dark:text-primary-900"
+      >
+        Nothing Found
+      </p>
+    </div>
   </div>
 </template>
