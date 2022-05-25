1be8614da702fd256c6f8b60d97c8ef447c430d3
behnam <salehinezhad.b@gmail.com>
2022-05-25 17:38:13 +0430

    minor changes

--- src/views/AlbumView.vue
+++ src/views/AlbumView.vue
@@ -50,204 +50,224 @@ function computeDuration(duration: number) {
   let seconds = zeroPad(duration % 60);
   return `${minutes}:${seconds}`;
 }
+
+function play() {
+  if (album.value) {
+    playerStore.playAlbum(album.value.id);
+  }
+}
 </script>
 
 <template>
-  <div class="font-Quicksand">
-    <div class="mx-2 lg:hidden">
-      <div class="mb-4 flex items-center">
+  <template v-if="album">
+    <div class="font-Quicksand">
+      <div class="mx-2 lg:hidden">
+        <div class="mb-4 flex items-center">
+          <div class="group relative">
+            <img
+              class="lazyload h-36 w-36 max-w-fit rounded-xl shadow-lg"
+              :data-src="`/img/${album?.thumbnail}`"
+              :alt="album?.title"
+            />
+            <PlayButton
+              class="bottom-2 right-2 group-hover:scale-100"
+              @click="play()"
+            />
+          </div>
+
+          <div class="ml-5">
+            <div
+              class="mb-4 break-all text-3xl font-bold text-secondary-900 transition-colors duration-300 dark:text-primary-900"
+            >
+              {{ album?.title }}
+            </div>
+
+            <div
+              class="flex items-center text-xs font-medium text-secondary-900 transition-colors duration-300 dark:text-primary-900"
+            >
+              <div>{{ album?.type }}</div>
+
+              <template v-if="album?.type !== 'Single'">
+                <div
+                  class="mx-3 h-1.5 w-1.5 rounded-full bg-secondary-900 transition-colors duration-300 dark:bg-primary-900"
+                ></div>
+                <div
+                  class="font-medium text-secondary-900 transition-colors duration-300 dark:text-primary-900"
+                >
+                  {{ album?.tracksCount }} Songs
+                </div>
+              </template>
+
+              <div class="flex items-center">
+                <div
+                  class="mx-3 h-1.5 w-1.5 rounded-full bg-secondary-900 transition-colors duration-300 dark:bg-primary-900"
+                ></div>
+                <div
+                  class="font-medium text-secondary-900 transition-colors duration-300 dark:text-primary-900"
+                >
+                  {{ album?.year }}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="flex flex-wrap">
+          <div v-for="(artist, index) in artists" :key="index">
+            <router-link
+              :to="'/artist/' + artist.id"
+              class="mr-2 mt-2 flex items-center rounded-full bg-primary-800 py-2 px-4 transition-colors duration-300 hover:bg-primary-700 dark:bg-secondary-800 dark:hover:bg-secondary-700"
+            >
+              <img
+                class="lazyload w-9 rounded-full"
+                :data-src="`/img/${artist.thumbnail}`"
+                :alt="artist.name"
+              />
+              <div
+                class="ml-2 font-normal text-secondary-900 transition-colors duration-300 dark:text-primary-900"
+              >
+                {{ artist.name }}
+              </div>
+            </router-link>
+          </div>
+        </div>
+      </div>
+
+      <div class="mx-4 hidden flex-row items-center lg:flex">
         <div class="group relative">
           <img
-            class="lazyload h-36 w-36 max-w-fit rounded-xl shadow-lg"
+            class="lazyload h-60 w-60 max-w-fit rounded-xl shadow-xl"
             :data-src="`/img/${album?.thumbnail}`"
             :alt="album?.title"
           />
           <PlayButton
-            class="bottom-2 right-2 group-hover:scale-100"
-            @click="playerStore.playAlbum(album?.id)"
+            class="bottom-4 right-4 group-hover:scale-125"
+            @click="play()"
           />
         </div>
 
-        <div class="ml-5">
+        <div class="grow pl-8 pr-4">
           <div
-            class="mb-4 break-all text-3xl font-bold text-secondary-900 transition-colors duration-300 dark:text-primary-900"
+            class="mb-8 flex flex-row items-center justify-between text-secondary-900 transition-colors duration-300 dark:text-primary-900"
           >
-            {{ album?.title }}
-          </div>
-
-          <div
-            class="flex items-center text-xs font-medium text-secondary-900 transition-colors duration-300 dark:text-primary-900"
-          >
-            <div>{{ album?.type }}</div>
-
-            <template v-if="album?.type !== 'Single'">
-              <div
-                class="mx-3 h-1.5 w-1.5 rounded-full bg-secondary-900 transition-colors duration-300 dark:bg-primary-900"
-              ></div>
-              <div
-                class="font-medium text-secondary-900 transition-colors duration-300 dark:text-primary-900"
-              >
-                {{ album?.tracksCount }} Songs
-              </div>
-            </template>
-
-            <div class="flex items-center">
-              <div
-                class="mx-3 h-1.5 w-1.5 rounded-full bg-secondary-900 transition-colors duration-300 dark:bg-primary-900"
-              ></div>
-              <div
-                class="font-medium text-secondary-900 transition-colors duration-300 dark:text-primary-900"
-              >
-                {{ album?.year }}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="flex flex-wrap">
-        <div v-for="(artist, index) in artists" :key="index">
-          <router-link
-            :to="'/artist/' + artist.id"
-            class="mr-2 mt-2 flex items-center rounded-full bg-primary-800 py-2 px-4 transition-colors duration-300 hover:bg-primary-700 dark:bg-secondary-800 dark:hover:bg-secondary-700"
-          >
-            <img
-              class="lazyload w-9 rounded-full"
-              :data-src="`/img/${artist.thumbnail}`"
-              :alt="artist.name"
-            />
-            <div
-              class="ml-2 font-normal text-secondary-900 transition-colors duration-300 dark:text-primary-900"
-            >
-              {{ artist.name }}
-            </div>
-          </router-link>
-        </div>
-      </div>
-    </div>
-
-    <div class="mx-4 hidden flex-row items-center lg:flex">
-      <div class="group relative">
-        <img
-          class="lazyload h-60 w-60 max-w-fit rounded-xl shadow-xl"
-          :data-src="`/img/${album?.thumbnail}`"
-          :alt="album?.title"
-        />
-        <PlayButton
-          class="bottom-4 right-4 group-hover:scale-125"
-          @click="playerStore.playAlbum(album?.id)"
-        />
-      </div>
-
-      <div class="grow pl-8 pr-4">
-        <div
-          class="mb-8 flex flex-row items-center justify-between text-secondary-900 transition-colors duration-300 dark:text-primary-900"
-        >
-          <div class="text-3xl font-bold">
-            {{ album?.title }}
-          </div>
-
-          <div class="flex items-center font-semibold">
-            <div
-              class="text-secondary-900 transition-colors duration-300 dark:text-primary-900"
-            >
-              {{ album?.type }}
+            <div class="text-3xl font-bold">
+              {{ album?.title }}
             </div>
 
-            <template v-if="album?.type !== 'Single'">
-              <div
-                class="mx-3 h-1.5 w-1.5 rounded-full bg-secondary-900 transition-colors duration-300 dark:bg-primary-900"
-              ></div>
+            <div class="flex items-center font-semibold">
               <div
                 class="text-secondary-900 transition-colors duration-300 dark:text-primary-900"
               >
-                {{ album?.tracksCount }} Songs
+                {{ album?.type }}
               </div>
-            </template>
-          </div>
-        </div>
 
-        <div class="flex items-center justify-between">
-          <div class="flex flex-wrap">
-            <div v-for="(artist, index) in artists" :key="index">
-              <router-link
-                :to="'/artist/' + artist.id"
-                class="mr-2 mt-2 flex items-center rounded-full bg-primary-800 py-2 px-4 transition-colors duration-300 hover:bg-primary-700 dark:bg-secondary-800 dark:hover:bg-secondary-700"
-              >
-                <img
-                  class="lazyload w-11 rounded-full"
-                  :data-src="`/img/${artist.thumbnail}`"
-                  :alt="artist.name"
-                />
+              <template v-if="album?.type !== 'Single'">
                 <div
-                  class="ml-2 font-normal text-secondary-900 transition-colors duration-300 dark:text-primary-900"
+                  class="mx-3 h-1.5 w-1.5 rounded-full bg-secondary-900 transition-colors duration-300 dark:bg-primary-900"
+                ></div>
+                <div
+                  class="text-secondary-900 transition-colors duration-300 dark:text-primary-900"
                 >
-                  {{ artist.name }}
+                  {{ album?.tracksCount }} Songs
                 </div>
-              </router-link>
+              </template>
             </div>
           </div>
 
-          <div
-            class="font-semibold text-secondary-900 transition-colors duration-300 dark:text-primary-900"
-          >
-            {{ album?.year }}
+          <div class="flex items-center justify-between">
+            <div class="flex flex-wrap">
+              <div v-for="(artist, index) in artists" :key="index">
+                <router-link
+                  :to="'/artist/' + artist.id"
+                  class="mr-2 mt-2 flex items-center rounded-full bg-primary-800 py-2 px-4 transition-colors duration-300 hover:bg-primary-700 dark:bg-secondary-800 dark:hover:bg-secondary-700"
+                >
+                  <img
+                    class="lazyload w-11 rounded-full"
+                    :data-src="`/img/${artist.thumbnail}`"
+                    :alt="artist.name"
+                  />
+                  <div
+                    class="ml-2 font-normal text-secondary-900 transition-colors duration-300 dark:text-primary-900"
+                  >
+                    {{ artist.name }}
+                  </div>
+                </router-link>
+              </div>
+            </div>
+
+            <div
+              class="font-semibold text-secondary-900 transition-colors duration-300 dark:text-primary-900"
+            >
+              {{ album?.year }}
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div
-      class="my-6 h-1 rounded-full bg-primary-500 transition-colors duration-300 dark:bg-secondary-500 lg:my-8"
-    ></div>
-
-    <div>
       <div
-        class="group relative flex items-center rounded-lg py-5 px-4 transition-colors duration-300 hover:bg-primary-800 dark:hover:bg-secondary-800 lg:mx-4"
-        v-for="(song, index) in songs"
-        :key="song.number"
-      >
+        class="my-6 h-1 rounded-full bg-primary-500 transition-colors duration-300 dark:bg-secondary-500 lg:my-8"
+      ></div>
+
+      <div>
         <div
-          class="ml-2 w-6 text-lg font-medium text-secondary-900 transition-colors duration-300 dark:text-primary-900 lg:ml-4"
+          class="group relative flex items-center rounded-lg py-5 px-4 transition-colors duration-300 hover:bg-primary-800 dark:hover:bg-secondary-800 lg:mx-4"
+          v-for="(song, index) in songs"
+          :key="song.number"
         >
-          {{ song.number }}
-        </div>
-        <div class="ml-4 overflow-hidden lg:ml-8">
           <div
-            class="mb-1 text-sm font-medium text-secondary-900 transition-colors duration-300 dark:text-primary-900 lg:text-base"
+            class="ml-2 w-6 text-lg font-medium text-secondary-900 transition-colors duration-300 dark:text-primary-900 lg:ml-4"
           >
-            {{ song.title }}
+            {{ song.number }}
+          </div>
+          <div class="ml-4 overflow-hidden lg:ml-8">
+            <div
+              class="mb-1 text-sm font-medium text-secondary-900 transition-colors duration-300 dark:text-primary-900 lg:text-base"
+            >
+              {{ song.title }}
+            </div>
+            <div
+              class="overflow-hidden text-ellipsis whitespace-nowrap text-xs text-secondary-500 transition-colors duration-300 dark:text-primary-500 lg:text-sm"
+            >
+              <template v-for="(artist, index) in song.artists" :key="index">
+                <router-link
+                  :to="'/artist/' + artist.id"
+                  class="underline-offset-2 hover:underline"
+                >
+                  {{ artist.name }}
+                </router-link>
+                <span
+                  v-if="
+                    song.artists.length !== 1 &&
+                    index !== song.artists.length - 1
+                  "
+                >
+                  ,
+                </span>
+              </template>
+            </div>
           </div>
           <div
-            class="overflow-hidden text-ellipsis whitespace-nowrap text-xs text-secondary-500 transition-colors duration-300 dark:text-primary-500 lg:text-sm"
+            class="ml-auto pl-2 text-sm font-medium text-secondary-900 transition-colors duration-300 dark:text-primary-900 lg:text-base"
           >
-            <template v-for="(artist, index) in song.artists" :key="index">
-              <router-link
-                :to="'/artist/' + artist.id"
-                class="underline-offset-2 hover:underline"
-              >
-                {{ artist.name }}
-              </router-link>
-              <span
-                v-if="
-                  song.artists.length !== 1 && index !== song.artists.length - 1
-                "
-              >
-                ,
-              </span>
-            </template>
+            {{ computeDuration(song.duration) }}
           </div>
+          <PlayButton
+            class="left-2 group-hover:scale-100 lg:left-4"
+            @click="playerStore.playSongs(songs, index)"
+          />
         </div>
-        <div
-          class="ml-auto pl-2 text-sm font-medium text-secondary-900 transition-colors duration-300 dark:text-primary-900 lg:text-base"
-        >
-          {{ computeDuration(song.duration) }}
-        </div>
-        <PlayButton
-          class="left-2 group-hover:scale-100 lg:left-4"
-          @click="playerStore.playSongs(songs, index)"
-        />
       </div>
     </div>
-  </div>
+  </template>
+  <template v-else>
+    <div
+      class="mx-5 mt-10 flex items-center justify-center rounded-lg bg-primary-800 py-32 transition-colors duration-300 dark:bg-secondary-800 lg:mx-48"
+    >
+      <p
+        class="text-center font-Roboto text-lg font-medium text-secondary-900 transition-colors duration-300 dark:text-primary-900"
+      >
+        Album Not Found
+      </p>
+    </div>
+  </template>
 </template>
--- src/views/ArtistView.vue
+++ src/views/ArtistView.vue
@@ -49,85 +49,104 @@ const popularSongs = computed(() => {
   }
   return songs;
 });
+
+function play() {
+  if (artist.value) {
+    playerStore.playArtist(artist.value.id);
+  }
+}
 </script>
 
 <template>
   <div>
-    <div class="flex flex-col items-center lg:flex-row">
-      <div class="group relative">
-        <img
-          class="lazyload mx-auto h-52 w-52 rounded-full shadow-xl lg:mx-0 lg:h-60 lg:w-60"
-          :data-src="`/img/${artist?.thumbnail}`"
-          :alt="artist?.name"
-        />
-        <PlayButton
-          class="bottom-3 right-3 group-hover:scale-125"
-          @click="playerStore.playArtist(artist?.id)"
-        />
-      </div>
-      <div
-        class="mt-4 flex flex-col items-center lg:mt-0 lg:items-start lg:pl-12"
-      >
-        <div
-          class="mb-4 font-Quicksand text-4xl font-bold text-secondary-900 transition-colors duration-300 dark:text-primary-900 lg:text-6xl"
-        >
-          {{ artist?.name }}
-        </div>
-        <div
-          class="font-Quicksand text-sm font-semibold text-neutral-400 lg:text-base"
-        >
-          {{ artist?.monthlyListeners }} Monthy Listeners
-        </div>
-      </div>
-    </div>
-
-    <template v-if="popularSongs.length">
-      <TitleBar text="Popular" />
-
-      <div
-        class="group relative mb-2 flex items-center rounded-lg py-2 font-Quicksand font-medium text-secondary-900 transition-colors duration-300 hover:bg-primary-800 dark:text-primary-900 dark:hover:bg-secondary-800 lg:py-3"
-        v-for="(song, index) in popularSongs"
-        :key="song.id"
-      >
-        <router-link
-          class="flex grow items-center"
-          :to="'/album/' + song.albumId"
-        >
-          <div class="ml-6 w-4 text-lg lg:ml-8">{{ index + 1 }}</div>
-
+    <template v-if="artist">
+      <div class="flex flex-col items-center lg:flex-row">
+        <div class="group relative">
           <img
-            class="lazyload mx-6 h-14 w-14 lg:mx-7 lg:h-16 lg:w-16"
-            :data-src="`/img/${song.thumbnail}`"
-            :alt="song.title"
+            class="lazyload mx-auto h-52 w-52 rounded-full shadow-xl lg:mx-0 lg:h-60 lg:w-60"
+            :data-src="`/img/${artist?.thumbnail}`"
+            :alt="artist?.name"
           />
-
-          <div class="lg:text-lg">
-            {{ song.title }}
+          <PlayButton
+            class="bottom-3 right-3 group-hover:scale-125"
+            @click="play()"
+          />
+        </div>
+        <div
+          class="mt-4 flex flex-col items-center lg:mt-0 lg:items-start lg:pl-12"
+        >
+          <div
+            class="mb-4 font-Quicksand text-4xl font-bold text-secondary-900 transition-colors duration-300 dark:text-primary-900 lg:text-6xl"
+          >
+            {{ artist?.name }}
           </div>
-        </router-link>
-
-        <PlayButton
-          class="left-2 group-hover:scale-100 lg:left-3"
-          @click="playerStore.playSongs(popularSongs, index)"
-        />
-      </div>
-    </template>
-
-    <template v-if="artistAlbums.length">
-      <TitleBar text="Albums" />
-      <div class="flex flex-wrap">
-        <div v-for="album in artistAlbums" :key="album.id">
-          <AlbumCard :album="album" />
+          <div
+            class="font-Quicksand text-sm font-semibold text-neutral-400 lg:text-base"
+          >
+            {{ artist?.monthlyListeners }} Monthy Listeners
+          </div>
         </div>
       </div>
-    </template>
 
-    <template v-if="artistSingles.length">
-      <TitleBar text="Singles" />
-      <div class="flex flex-wrap">
-        <div v-for="album in artistSingles" :key="album.id">
-          <AlbumCard :album="album" />
+      <template v-if="popularSongs.length">
+        <TitleBar text="Popular" />
+
+        <div
+          class="group relative mb-2 flex items-center rounded-lg py-2 font-Quicksand font-medium text-secondary-900 transition-colors duration-300 hover:bg-primary-800 dark:text-primary-900 dark:hover:bg-secondary-800 lg:py-3"
+          v-for="(song, index) in popularSongs"
+          :key="song.id"
+        >
+          <router-link
+            class="flex grow items-center"
+            :to="'/album/' + song.albumId"
+          >
+            <div class="ml-6 w-4 text-lg lg:ml-8">{{ index + 1 }}</div>
+
+            <img
+              class="lazyload mx-6 h-14 w-14 lg:mx-7 lg:h-16 lg:w-16"
+              :data-src="`/img/${song.thumbnail}`"
+              :alt="song.title"
+            />
+
+            <div class="lg:text-lg">
+              {{ song.title }}
+            </div>
+          </router-link>
+
+          <PlayButton
+            class="left-2 group-hover:scale-100 lg:left-3"
+            @click="playerStore.playSongs(popularSongs, index)"
+          />
         </div>
+      </template>
+
+      <template v-if="artistAlbums.length">
+        <TitleBar text="Albums" />
+        <div class="flex flex-wrap">
+          <div v-for="album in artistAlbums" :key="album.id">
+            <AlbumCard :album="album" />
+          </div>
+        </div>
+      </template>
+
+      <template v-if="artistSingles.length">
+        <TitleBar text="Singles" />
+        <div class="flex flex-wrap">
+          <div v-for="album in artistSingles" :key="album.id">
+            <AlbumCard :album="album" />
+          </div>
+        </div>
+      </template>
+    </template>
+    <template v-else>
+      <div
+        class="mx-5 mt-10 flex items-center justify-center rounded-lg bg-primary-800 py-32 transition-colors duration-300 dark:bg-secondary-800 lg:mx-48"
+      >
+        <p
+          class="text-center font-Roboto text-lg font-medium text-secondary-900 transition-colors duration-300 dark:text-primary-900"
+        >
+          Artist Not Found
+        </p>
       </div>
     </template>
   </div>
